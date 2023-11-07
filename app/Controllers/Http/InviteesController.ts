9378975e0abf6f11c5da5c31e36c9de5e7fb937c
Afrijal Dzuhri <ijuldz@gmail.com>
2023-11-08 00:32:12 +0700

    init project

---
+++ app/Controllers/Http/InviteesController.ts
@@ -0,0 +1,125 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Invitee from 'App/Models/Invitee'
+
+export default class InviteesController {
+  private makeid(length) {
+    let result = ''
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const charactersLength = characters.length
+    let counter = 0
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+      counter += 1
+    }
+    return result
+  }
+
+  public async list(ctx: HttpContextContract) {
+    try {
+      const data = await Invitee.all()
+
+      return ctx.response.json({
+        data,
+      })
+    } catch (error) {
+      console.log(error)
+      return ctx.response.json({
+        error: error,
+      })
+    }
+  }
+
+  public async detail({ request, response }: HttpContextContract) {
+    const id = request.param('id')
+
+    try {
+      const data = await Invitee.find(id)
+
+      return response.json({
+        data,
+      })
+    } catch (error) {
+      console.log(error)
+      return response.json({
+        error,
+      })
+    }
+  }
+
+  public async create({ response, request }: HttpContextContract) {
+    const name = request.input('name')
+
+    try {
+      const newPackage = new Invitee()
+      newPackage.name = name
+      newPackage.hash = this.makeid(5)
+
+      const data = await newPackage.save()
+
+      return response.status(201).json({
+        data: data,
+      })
+    } catch (error) {
+      console.log(error)
+
+      return response.json({
+        error: error,
+      })
+    }
+  }
+
+  public async edit({ request, response }: HttpContextContract) {
+    const id = request.param('id')
+    const name = request.input('name')
+
+    try {
+      const newData = await Invitee.find(id)
+
+      if (newData) {
+        newData.name = name
+
+        const data = await newData.save()
+
+        return response.json({
+          data: data,
+        })
+      } else {
+        return response.json({
+          data: 'Data not found',
+        })
+      }
+    } catch (error) {
+      console.log(error)
+
+      return response.json({
+        error,
+      })
+    }
+  }
+
+  public async delete({ request, response }: HttpContextContract) {
+    const id = request.param('id')
+
+    try {
+      const data = await Invitee.find(id)
+
+      if (data) {
+        await data.delete()
+
+        return response.json({
+          data: 'Data successfully deleted',
+        })
+      } else {
+        return response.json({
+          data: 'Data successfully deleted',
+        })
+      }
+    } catch (error) {
+      console.log(error)
+
+      return response.json({
+        error,
+      })
+    }
+  }
+}
---
+++ start/routes.ts
@@ -0,0 +1,7 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', 'InviteesController.list')
+Route.get('/:id', 'InviteesController.detail')
+Route.post('/', 'InviteesController.create')
+Route.put('/:id', 'InviteesController.edit')
+Route.delete('/:id', 'InviteesController.remove')
